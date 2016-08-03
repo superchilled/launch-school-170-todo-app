660d66f4835f9a4d3118bbfe97eff679d82dcd29
superchilled <karl@superchilled.co.uk>
2016-08-03 14:32:44 +0100

    Adding new list, flash message, validation features

---
+++ todo.rb
@@ -0,0 +1,41 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View all the lists
+get "/lists" do
+  @lists = session[:lists]
+
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Create a new list
+post '/lists' do
+  list_name = params[:list_name].strip
+  if (1..100).cover?(list_name.size)
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  else
+    session[:error] = "The list name must be between 1 and 100 characters."
+    erb :new_list, layout: :layout
+  end
+end
